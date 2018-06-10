9bc75f08c09c7f54456ca655071238c55595db4e
Wojtek <rybinski05@gmail.com>
2018-06-10 21:28:07 +0200

    Added projects page

--- server.js
+++ server.js
@@ -55,6 +55,12 @@ app.get('/bad', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects page',
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server is up on ${port}`)
 });
